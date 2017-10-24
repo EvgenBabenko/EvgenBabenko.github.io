f57f06b4bed542bd9c664d864cae590f7871d77b
EvgenBabenko <__>
2017-10-24 09:26:40 +0300

    Add work Ad

---
+++ works/ad/src/js/init.js
@@ -0,0 +1,26 @@
+// jshint esversion: 6
+
+function init() {
+  'use strict';
+
+  function ready() {
+    let iframe = document.createElement('iframe');
+  
+    iframe.style.width = '970px';
+    iframe.style.height = '250px';
+    iframe.style.border = 'none';
+    iframe.src = 'build/index.html';
+    iframe.style.display = 'block';
+    iframe.style.position = 'absolute';
+    iframe.style.top = '20px';
+    iframe.style.left = '50px';
+    // iframe.style.zIndex = '9999';
+
+
+    document.body.appendChild(iframe);
+  }
+
+  document.addEventListener("DOMContentLoaded", ready);
+}
+
+init();
