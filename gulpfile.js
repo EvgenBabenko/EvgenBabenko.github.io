aa7eb2c0af64670e49b7e4f44b7a7151655ee980
EvgenBabenko <evgen.babenko@gmail.com>
2018-09-25 10:08:58 +0300

    added new work - task-manager-bj

--- gulpfile.js
+++ gulpfile.js
@@ -1,134 +1,134 @@
 // 'use strict';
-var gulp = require('gulp'),
-	del = require('del'),
-	sass = require('gulp-sass'),
-	browserSync = require('browser-sync'),
-	cleanCSS = require('gulp-clean-css'),
-	imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
-	cache = require('gulp-cache'),
-	uglify = require('gulp-uglify'),
-	rigger = require('gulp-rigger'),
-	wait = require('gulp-wait');
+const gulp = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const cache = require('gulp-cache');
+// const uglify = require('gulp-uglify');
+const rigger = require('gulp-rigger');
+const wait = require('gulp-wait');
 
-var path = {
-	build: { //put files to
-		html: 'build/',
-		css: 'build/css/',
-		img: 'build/img/',
-		fonts: 'build/fonts/',
-		js: 'build/js/'
-	},
+const path = {
+  // put files to
+  build: {
+    html: 'build/',
+    css: 'build/css/',
+    img: 'build/img/',
+    fonts: 'build/fonts/',
+    js: 'build/js/'
+  },
 
-	src: { //get files from 
-		html: 'src/*.html',
-		sass: 'src/style/main.+(scss|sass)',
-		img: [
-			'src/img/**/*.*',
-			'!src/img/icon/*.*'
-		],
-		fonts: 'src/fonts/*.*',
-		js: 'src/js/*.js'
-	},
+  // get files from
+  src: {
+    html: 'src/*.html',
+    sass: 'src/style/main.+(scss|sass)',
+    img: ['src/img/**/*.*', '!src/img/icon/*.*'],
+    fonts: 'src/fonts/*.*',
+    js: 'src/js/*.js'
+  },
 
-	watch: {
-		html: [
-			'src/*.html',
-			'src/template/*.html'
-		],
-		sass: 'src/style/**/*.+(scss|sass)',
-		img: 'src/img/**/*.*',
-		fonts: 'src/fonts/*.*',
-		js: 'src/js/**/*.js'
-	}
+  watch: {
+    html: ['src/*.html', 'src/template/*.html'],
+    sass: 'src/style/**/*.+(scss|sass)',
+    img: 'src/img/**/*.*',
+    fonts: 'src/fonts/*.*',
+    js: 'src/js/**/*.js'
+  }
 };
 
-var config = {
-	server: {
-		baseDir: 'build/'
-	},
-	tunnel: true,
-	host: 'localhost',
-	port: 9000,
-	logPrefix: "Frontend_Devil",
-	// notify: false // Отключаем уведомления
+const config = {
+  server: {
+    baseDir: 'build/'
+  },
+  tunnel: true,
+  host: 'localhost',
+  port: 9000,
+  logPrefix: 'Frontend_Devil'
+  // notify: false // Отключаем уведомления
 };
 
-gulp.task('html', function () {
-	gulp.src(path.src.html)
-		.pipe(rigger())
-		.pipe(gulp.dest(path.build.html))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task('html', () => {
+  gulp
+    .src(path.src.html)
+    .pipe(rigger())
+    .pipe(gulp.dest(path.build.html))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('sass', function () {
-	gulp.src(path.src.sass)
-		.pipe(wait())
-		.pipe(sass(
-			// {
-			// 	outputStyle: 'nested'
-			// }
-		))
-		.pipe(cleanCSS(
-			{
-				format: 'beautify',
-				level: '2'
-			}
-		))
-		.pipe(gulp.dest(path.build.css))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task('sass', () => {
+  gulp
+    .src(path.src.sass)
+    .pipe(wait())
+    .pipe(
+      sass({
+        // outputStyle: 'nested'
+      })
+    )
+    .pipe(
+      cleanCSS({
+        format: 'beautify',
+        level: '2'
+      })
+    )
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('img', function () {
-	gulp.src(path.src.img)
-		.pipe(cache(imagemin({
-			interlaced: true,
-			progressive: true,
-			svgoPlugins: [{ removeViewBox: false }],
-			use: [pngquant()]
-		})))
-		.pipe(gulp.dest(path.build.img))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task('img', () => {
+  gulp
+    .src(path.src.img)
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true,
+          progressive: true,
+          svgoPlugins: [{ removeViewBox: false }],
+          use: [pngquant()]
+        })
+      )
+    )
+    .pipe(gulp.dest(path.build.img))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('js', function () {
-	gulp.src(path.src.js)
-		// .pipe(rigger())
-		// .pipe(uglify())
-		.pipe(gulp.dest(path.build.js))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task('js', () => {
+  gulp
+    .src(path.src.js)
+    // .pipe(rigger())
+    // .pipe(uglify())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task('webserver', function () {
-	browserSync(config);
+gulp.task('webserver', () => {
+  browserSync(config);
 });
 
-gulp.task('watch', function () {
-	gulp.watch(path.watch.html, ['html']);
-	gulp.watch(path.watch.sass, ['sass']);
-	gulp.watch(path.watch.img, ['img']);
-	gulp.watch(path.watch.fonts, ['fonts']);
-	gulp.watch(path.watch.js, ['js']);
+gulp.task('watch', () => {
+  gulp.watch(path.watch.html, ['html']);
+  gulp.watch(path.watch.sass, ['sass']);
+  gulp.watch(path.watch.img, ['img']);
+  gulp.watch(path.watch.fonts, ['fonts']);
+  gulp.watch(path.watch.js, ['js']);
 });
 
-gulp.task('clean', function() {
-	del.sync('build');
+gulp.task('clean', () => {
+  del.sync('build');
 });
 
-gulp.task('clear', function () {
-	cache.clearAll();
+gulp.task('clear', () => {
+  cache.clearAll();
 });
 
 gulp.task('build', [
-	'html',
-	'sass',
-	'img',
-	// 'fonts',
-	'js'
+  'html',
+  'sass',
+  'img',
+  // 'fonts',
+  'js'
 ]);
 
-gulp.task('default', [
-	'build',
-	'watch',
-	'webserver'
-]);
+gulp.task('default', ['build', 'watch', 'webserver']);
